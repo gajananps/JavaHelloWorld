00e63132b2c45130fb255aa2feb8750ef8f5e15a
Gajanan PS <gajananps@yahoo.com>
2024-08-26 18:13:25 +0530

    Update HelloWorld.java
    Signed-off-by: Gajanan PS <gajananps@yahoo.com>

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@
 Date : 1-Nov-2021
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
 }
